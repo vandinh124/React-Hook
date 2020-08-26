6f124a912ebf2000756d37a23a0fffea423e0d6d
vandinh124 <vanvdinh124@gmail.com>
2020-08-25 20:13:57 -0700

    finish dropdown

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 // import Accordion from './components/Accordion';
-import Search from './components/Search';
+// import Search from './components/Search';
+import Dropdown from './components/Dropdown';
 
 // const items = [
 // 	{
@@ -17,11 +18,37 @@ import Search from './components/Search';
 // 	}
 // ];
 
+	const options = [
+		{
+			label: 'The Color Red',
+			value :'red'
+		},
+		{
+			label: 'The Color Green',
+			value :'green'
+		},
+		{
+			label: 'The Color Blue',
+			value :'blue'
+		},
+	]
 export default () => {
+	const [selected, setSelected] = useState(options[0])
+	const [showDropdown, setShowDropdown] = useState(true)
+
 	return (
 		<div>
 			{/* <Accordion items={items} /> */}
-			<Search />
+			{/* <Search /> */}
+			<button onClick={() => setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+			{showDropdown ?
+				<Dropdown 
+				selected={selected}
+				onSelectedChange={setSelected}
+				options={options}
+			/>: null
+			}
+			
 		</div>
 	);
 };
